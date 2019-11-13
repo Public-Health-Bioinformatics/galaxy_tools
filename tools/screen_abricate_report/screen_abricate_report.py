c27095ceaf23aa2c910e0ea866db9ec8d3e7816b
Dan Fornika <dfornika@gmail.com>
2019-11-12 18:34:25 -0800

    Fix some linting & testing issues for 'screen_abricate_report' (#4)
    * Appease the linter
    
    * Shorten long lines
    
    * Appease the linter
    
    * Appease the linter
    
    * Appease the linter
    
    * Appease the linter
    
    * Appease the linter
    
    * Appease the linter
    
    * Appease the linter
    
    * Appease the linter
    
    * Appease the linter
    
    * Trigger new build

--- tools/screen_abricate_report/screen_abricate_report.py
+++ tools/screen_abricate_report/screen_abricate_report.py
@@ -3,11 +3,9 @@
 from __future__ import print_function
 
 import argparse
-import os
-import re
-import sys
 import csv
-from pprint import pprint
+import re
+
 
 def parse_screen_file(screen_file):
     screen = []
@@ -17,21 +15,26 @@ def parse_screen_file(screen_file):
             screen.append(row)
     return screen
 
+
 def get_fieldnames(input_file):
     with open(input_file) as f:
         reader = csv.DictReader(f, delimiter="\t", quotechar='"')
         row = next(reader)
     fieldnames = row.keys()
     return fieldnames
-    
+
+
 def main(args):
     screen = parse_screen_file(args.screening_file)
-    abricate_report_fieldnames = get_fieldnames(args.abricate_report)
     gene_detection_status_fieldnames = ['gene_name', 'detected']
-    with open(args.abricate_report, 'r') as f1, open(args.screened_report, 'w') as f2, open(args.gene_detection_status, 'w') as f3:
+    with open(args.abricate_report, 'r') as f1, \
+            open(args.screened_report, 'w') as f2, \
+            open(args.gene_detection_status, 'w') as f3:
         abricate_report_reader = csv.DictReader(f1, delimiter="\t", quotechar='"')
-        screened_report_writer = csv.DictWriter(f2, delimiter="\t", quotechar='"', fieldnames=abricate_report_reader.fieldnames)
-        gene_detection_status_writer = csv.DictWriter(f3, delimiter="\t", quotechar='"', fieldnames=gene_detection_status_fieldnames)
+        screened_report_writer = csv.DictWriter(f2, delimiter="\t", quotechar='"',
+                                                fieldnames=abricate_report_reader.fieldnames)
+        gene_detection_status_writer = csv.DictWriter(f3, delimiter="\t", quotechar='"',
+                                                      fieldnames=gene_detection_status_fieldnames)
         screened_report_writer.writeheader()
         gene_detection_status_writer.writeheader()
 
@@ -45,14 +48,16 @@ def main(args):
                     gene_detection_status['detected'] = True
                     screened_report_writer.writerow(abricate_report_row)
             gene_detection_status_writer.writerow(gene_detection_status)
-            f1.seek(0) # return file pointer to start of abricate report
+            f1.seek(0)  # return file pointer to start of abricate report
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("abricate_report", help="Input: Abricate report to screen (tsv)")
     parser.add_argument("--screening_file", help="Input: List of genes to screen for (tsv)")
-    parser.add_argument("--screened_report", help="Output: Screened abricate report including only genes of interest (tsv)")
-    parser.add_argument("--gene_detection_status", help="Output: detection status for all genes listed in the screening file (tsv)")
+    parser.add_argument("--screened_report", help=("Output: Screened abricate report ",
+                                                   "including only genes of interest (tsv)"))
+    parser.add_argument("--gene_detection_status", help=("Output: detection status for all genes ",
+                                                         "listed in the screening file (tsv)"))
     args = parser.parse_args()
     main(args)
