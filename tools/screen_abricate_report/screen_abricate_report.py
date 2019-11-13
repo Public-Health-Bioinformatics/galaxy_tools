b086611db5d0ac078d62e3122e972f6b3335a699
Dan Fornika <dfornika@gmail.com>
2019-11-12 16:55:07 -0800

    Appease the linter

--- tools/screen_abricate_report/screen_abricate_report.py
+++ tools/screen_abricate_report/screen_abricate_report.py
@@ -28,8 +28,8 @@ def main(args):
     screen = parse_screen_file(args.screening_file)
     gene_detection_status_fieldnames = ['gene_name', 'detected']
     with open(args.abricate_report, 'r') as f1, \
-         open(args.screened_report, 'w') as f2, \
-         open(args.gene_detection_status, 'w') as f3:
+            open(args.screened_report, 'w') as f2, \
+                open(args.gene_detection_status, 'w') as f3:
         abricate_report_reader = csv.DictReader(f1, delimiter="\t", quotechar='"')
         screened_report_writer = csv.DictWriter(f2, delimiter="\t", quotechar='"',
                                                 fieldnames=abricate_report_reader.fieldnames)
