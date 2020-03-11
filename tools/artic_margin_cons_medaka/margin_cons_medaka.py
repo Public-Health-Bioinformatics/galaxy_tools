cdc887baab41af2b386b1ec7239b55d474c0fff5
Dan Fornika <dfornika@gmail.com>
2020-03-10 20:30:17 -0700

    Update margin_cons_medaka

--- tools/artic_margin_cons_medaka/margin_cons_medaka.py
+++ tools/artic_margin_cons_medaka/margin_cons_medaka.py
@@ -20,31 +20,31 @@ from Bio import SeqIO
 def read_bed_file(fn):
     bedfile = []
     with open(fn) as csvfile:
-	reader = csv.reader(csvfile, dialect='excel-tab')
-	for row in reader:
-	    bedrow = {}
-	    bedrow['Primer_ID'] = row[3]
+        reader = csv.reader(csvfile, dialect='excel-tab')
+        for row in reader:
+            bedrow = {}
+            bedrow['Primer_ID'] = row[3]
 
-	    if len(row) >= 6:
-		# new style bed
-		bedrow['direction'] = row[5]
-	    elif len(row) == 5:
-		# old style without directory
-		if 'LEFT' in row[3]:
-		    bedrow['direction'] = '+'
-		elif 'RIGHT' in row[3]:
-		    bedrow['direction'] = '-'
-		else:
-		    print("Malformed BED file!", file=sys.stderr) 
-		    raise SystemExit
+            if len(row) >= 6:
+                # new style bed
+                bedrow['direction'] = row[5]
+            elif len(row) == 5:
+                # old style without directory
+                if 'LEFT' in row[3]:
+                    bedrow['direction'] = '+'
+                elif 'RIGHT' in row[3]:
+                    bedrow['direction'] = '-'
+                else:
+                    print("Malformed BED file!", file=sys.stderr) 
+                    raise SystemExit
 
-	    if bedrow['direction'] == '+':
-		bedrow['end'] = int(row[2])
-		bedrow['start'] = int(row[1])
-	    else:
-		bedrow['end'] = int(row[1])
-		bedrow['start'] = int(row[2])
-	    bedfile.append(bedrow)
+            if bedrow['direction'] == '+':
+                bedrow['end'] = int(row[2])
+                bedrow['start'] = int(row[1])
+            else:
+                bedrow['end'] = int(row[1])
+                bedrow['start'] = int(row[2])
+            bedfile.append(bedrow)
     return bedfile
 
 
