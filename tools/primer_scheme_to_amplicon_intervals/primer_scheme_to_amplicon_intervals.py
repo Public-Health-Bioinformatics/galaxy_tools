e4a0e4a7a9dde99965c32cfe502274631888e114
Dan Fornika <dfornika@gmail.com>
2020-03-17 19:40:49 -0700

    Improve primer scheme parsing

--- tools/primer_scheme_to_amplicon_intervals/primer_scheme_to_amplicon_intervals.py
+++ tools/primer_scheme_to_amplicon_intervals/primer_scheme_to_amplicon_intervals.py
@@ -8,18 +8,33 @@ import json
 
 
 def parse_primer_scheme(primer_scheme):
-    primer_pairs = []
+    primer_pairs = {}
     fieldnames = [
         'ref',
         'start',
         'end',
         'primer_name',
         'pool',
-    ] 
+    ]
+    primer_pair_names = set()
     with open(primer_scheme) as f:
         reader = csv.DictReader(f, delimiter="\t", quotechar='"', fieldnames=fieldnames)
         for row in reader:
-            primer_pairs.append(row)
+            pair_name = '_'.join(row['primer_name'].split('_')[0:2])
+            left_right = row['primer_name'].split('_')[2].lower()
+            if pair_name not in primer_pairs.keys():
+                primer_pairs[pair_name] = {}
+                primer_pairs[pair_name][left_right] = {
+                    'start': int(row['start']),
+                    'end': int(row['end']),
+                }
+            else:
+                primer_pairs[pair_name][left_right] = {
+                    'start': int(row['start']),
+                    'end': int(row['end']),
+                }
+            
+
     return primer_pairs
 
 
