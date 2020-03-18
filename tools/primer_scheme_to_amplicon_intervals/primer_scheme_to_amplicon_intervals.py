a0982235f27e688a19733cfaaaf3ec2cfacb6312
Dan Fornika <dfornika@gmail.com>
2020-03-17 19:47:12 -0700

    Complete conversion of primer scheme to amplicon intervals

--- tools/primer_scheme_to_amplicon_intervals/primer_scheme_to_amplicon_intervals.py
+++ tools/primer_scheme_to_amplicon_intervals/primer_scheme_to_amplicon_intervals.py
@@ -23,7 +23,7 @@ def parse_primer_scheme(primer_scheme):
             pair_name = '_'.join(row['primer_name'].split('_')[0:2])
             left_right = row['primer_name'].split('_')[2].lower()
             if pair_name not in primer_pairs.keys():
-                primer_pairs[pair_name] = {}
+                primer_pairs[pair_name] = {'ref': row['ref']}
                 primer_pairs[pair_name][left_right] = {
                     'start': int(row['start']),
                     'end': int(row['end']),
@@ -34,13 +34,17 @@ def parse_primer_scheme(primer_scheme):
                     'end': int(row['end']),
                 }
             
-
     return primer_pairs
 
 
 def main(args):
     primer_pairs = parse_primer_scheme(args.primer_scheme)
-    print(json.dumps(primer_pairs))
+    # print(json.dumps(primer_pairs))
+    for name, primers in primer_pairs.items():
+        ref = primers['ref']
+        start = str(primers['left']['start'])
+        end = str(primers['right']['end'])
+        print('\t'.join([ref, start, end, name]))
 
 
 if __name__ == '__main__':
